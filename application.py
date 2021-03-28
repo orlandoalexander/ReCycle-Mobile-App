41ed5a5cb11f564849fc283b002a409afbf91e4d
orlandoalexander <67097862+orlandoalexander@users.noreply.github.com>
2021-03-28 21:52:33 +0100

    Update application.py

--- application.py
+++ application.py
@@ -15,15 +15,7 @@ mycursor = mydb.cursor()  # initialises a cursor which allows you to communicate
 
 @application.route("/")
 def test():
-    output = []
-    input = list(("I want a pizza box").split(" "))
-    for i in input:
-        query = ("SELECT * FROM Nouns WHERE noun='%s'" % (i))
-        mycursor.execute(query) # returns all the values in the column 'noun' that match i.
-        if len(mycursor.fetchall()) > 0:  # if the MySQL execution returns a value, the word is a noun and so is added to the keywords list.
-            output.append(i.capitalize())
-    output = ",".join(output)
-    return output
+    return "Working"
 
 class returnNouns(Resource): #class that is a resource - for GET, PUT and DELETE requests
     def put(self): #function which is run when get request is made to the URL
@@ -36,7 +28,6 @@ class returnNouns(Resource): #class that is a resource - for GET, PUT and DELETE
                 self.output.append(i.capitalize())
         self.output = ",".join(self.output)
         return self.output
-
 
 
 api.add_resource(returnNouns, "/nouns") #adds the class 'returnNouns' to the Api as the class is a resource. This resource is found by making a GET request to the URL followed by "/nouns"
