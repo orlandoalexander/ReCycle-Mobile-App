6783af789959432f64b55f7d4034e1616151ead0
orlandoalexander <67097862+orlandoalexander@users.noreply.github.com>
2021-05-07 22:10:06 +0100

    Update application.py

--- application.py
+++ application.py
@@ -22,9 +22,9 @@ class returnRecyclingInfo(Resource): #class that is a resource - for GET, PUT an
     def post(self): #function which is run when get request is made to the URL
         self.input = list((request.form["input"]).split(" ")) #self.input = request.form["input"] # data is in JSON format. JSON file format is essentially a Python dictionary. The returned format must be 'JSON serializable' (i.e. in a valid JSON format - a dictionary)
         self.county = request.form["county"]
-        data = RecyclingInfo().noun_finder(self.input)
+        #data = RecyclingInfo().noun_finder(self.input)
         print("hello")
-        return data
+        return "hello"
         
         
 class RecyclingInfo():
