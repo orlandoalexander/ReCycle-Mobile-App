98c3b6d0db86ca68d77566ee02ebd04cc3632273
orlandoalexander <67097862+orlandoalexander@users.noreply.github.com>
2021-05-08 01:01:24 +0100

    Update application.py

--- application.py
+++ application.py
@@ -32,7 +32,7 @@ class RecyclingInfo():
         print(input)
         self.keywords = []
         for word in input:
-            query = ("SELECT * FROM Keywords WHERE Keyword ='%s'" % (word))
+            query = (SELECT * FROM Keywords WHERE Keyword ='%s' % (word))
             mycursor.execute(query) # returns all the values in the column 'Keyword' that match 'word'.
             if len(mycursor.fetchall()) > 0:  # if the MySQL execution returns a value, the word is a noun and so is added to the keywords list.
                 synonym_list = self.synonym_finder(word)
